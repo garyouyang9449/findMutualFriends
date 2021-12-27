041184696da1a2e948bc644b02d354a67ad41266
garyouyang9449 <garyouyang9449@gmail.com>
2021-12-27 12:30:09 -0800

    added twitter token

---
+++ server/auth/auth.ts
@@ -0,0 +1,5 @@
+export default {
+  'header': {
+    "Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAALeuXQEAAAAAeZTtwWjGLwG86k4r7GQyCMhtnUs%3DiVucZGDLbrTxgusjRx2Sds9LEGcJ9MowmwBlDj1PJyFzeuXgal"
+  }
+}
