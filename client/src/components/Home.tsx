0aa0bcc6c64144f877524ecfe617f4a6855cdd01
garyouyang9449 <garyouyang9449@gmail.com>
2021-12-27 12:12:43 -0800

    added proxy on client to connect to the server

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header/Header';
 import TwitterUsers from './TwitterUsers/TwitterUsers';
 
 const Home = () => {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("/mutual-followings")
+      .then((res) => res.json())
+      .then((data) => console.log(data.message));
+  }, []);
 
   return (
     <>
--- server/App.ts
+++ server/App.ts
@@ -1,9 +1,13 @@
 import express from "express";
 const app = express();
-const port = 8080;
+const PORT = 8080;
 
-app.get('/', function (req, res) {
-  res.send(' world')
+app.get('/mutual-followings', function (req, res) {
+  res.json({
+    message: 'in server!'
+  })
 });
 
-app.listen(port);
+app.listen(PORT, () => {
+  console.log(`running on PORT ${PORT}`)
+});
